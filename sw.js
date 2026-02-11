09a6fdd8f8d6a69c5cb065c6a7d7c1abe01e768f
1t01x1w4 <2073046979@qq.com>
2026-02-11 22:53:31 +0800

    Site updated: 2026-02-11 22:53:31

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1770820841967";const preCache = ["/images/taichi.png","/images/bannner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1770821485941";const preCache = ["/images/taichi.png","/images/bannner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
